3dc197db8eb591572fd6c473500094e3985fafe7
vmartin3 <vernonmartin11@gmail.com>
2016-06-15 21:05:25 -0400

    Version 2.0: A stable build with extra credit features implemented

--- TrueFalseStarter/QuestionsModel.swift
+++ TrueFalseStarter/QuestionsModel.swift
@@ -11,17 +11,16 @@ import GameKit
 
 struct QuestionsModel {
     let triviaQuestions: [[String : String]] = [
-        ["Question": "This was the only US President to serve more than two consecutive terms.", "Answer": "Franklin D. Roosevelt"],
-        ["Question": "Which of the following countries has the most residents?", "Answer": "Nigeria"],
-        ["Question": "In what year was the United Nations founded?", "Answer": "1945"],
-        ["Question": "The Titanic departed from the United Kingdom, where was it supposed to arrive?", "Answer": "New York City"],
-        ["Question": "Which nation produces the most oil?", "Answer": "Canada"],
-        ["Question": "Which country has most recently won consecutive World Cups in Soccer?", "Answer": "Brazil"],
-        ["Question": "Which of the following rivers is longest?", "Answer": "Mississippi"],
-        ["Question": "Which city is the oldest?", "Answer": "Mexico City"],
-        ["Question": "Which country was the first to allow women to vote in national elections?", "Answer": "Poland"],
-        ["Question": "Which city is the oldest?", "Answer": "Mexico City"],
-        ["Question": "Which of these countries won the most medals in the 2012 Summer Games?", "Answer": "Great Britian"]
+        ["Question": "This was the only US President to serve more than two consecutive terms.", "Answer": "Franklin D. Roosevelt", "Option2":"George Washington","Option3":"Woodrow Wilsdon","Option4":"Andrew Jack"],
+        ["Question": "Which of the following countries has the most residents?", "Answer": "Nigeria", "Option2":"Russia","Option3":"Iran","Option4":"Vietnam"],
+        ["Question": "In what year was the United Nations founded?", "Answer": "1945","Option2":"1918","Option3":"1919","Option4":"1954"],
+        ["Question": "The Titanic departed from the United Kingdom, where was it supposed to arrive?", "Answer": "New York City","Option2":"Paris","Option3":"Washington D.C.","Option4":"Boston"],
+        ["Question": "Which nation produces the most oil?", "Answer": "Canada","Option2":"Iran","Option3":"Iraq","Option4":"Brazil"],
+        ["Question": "Which country has most recently won consecutive World Cups in Soccer?", "Answer": "Brazil","Option2":"Italy","Option3":"Argetina","Option4":"Spain"],
+        ["Question": "Which of the following rivers is longest?", "Answer": "Mississippi","Option2":"Yangtze","Option3":"Congo","Option4":"Mekong"],
+        ["Question": "Which city is the oldest?", "Answer": "Mexico City","Option2":"Cape Town","Option3":"San Juan","Option4":"Sydney"],
+        ["Question": "Which country was the first to allow women to vote in national elections?", "Answer": "Poland","Option2":"United States","Option3":"Sweden","Option4":"Senegal"],
+        ["Question": "Which of these countries won the most medals in the 2012 Summer Games?", "Answer": "Great Britian","Option2":"France","Option3":"Germany","Option4":"Japan"]
     ]
     
     func getRandomQuestion() -> Dictionary<String,String>{
--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -9,41 +9,43 @@
 import UIKit
 import GameKit
 import AudioToolbox
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var questionField: UILabel!
     @IBOutlet weak var choice1: UIButton!
     @IBOutlet weak var choice2: UIButton!
     @IBOutlet weak var choice3: UIButton!
     @IBOutlet weak var choice4: UIButton!
+    @IBOutlet weak var displayCorrectAnswerField: UILabel!
     
-    @IBAction func choiceOneSelected(sender: AnyObject) {
-    }
-    @IBAction func choiceTwoSelected(sender: AnyObject) {
-    }
-    @IBAction func choiceThreeSelected(sender: AnyObject) {
-    }
-    @IBAction func choiceFourSelected(sender: AnyObject) {
-    }
-    
-
-    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var timer: UILabel!
+    let correctSoundURL =  NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("correct", ofType: "wav")!)
+    let wrongSoundURL =  NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("wrong", ofType: "wav")!)
+    var audioPlayer = AVAudioPlayer()
+    var audioPlayer2 = AVAudioPlayer()
     var indexOfSelectedQuestion: Int = 0
     var choices: [UIButton] = []
     var usedQuestions: [Int] = []
-    var totalNumOfQuestionsUsed = 0
+    var usedOptions: [Int] = []
     var settings = GameSettingsModel()
     let questionSet = QuestionsModel()
+    var gameTimer = NSTimer()
+    var timerCounter = 15
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         choices = [choice1,choice2, choice3, choice4]
         loadGameStartSound()
+        
         // Start game
         playGameStartSound()
         displayQuestion()
+        displayAnswerOptions()
+        startTimer()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,33 +53,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func displayQuestion() {
-        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(questionSet.triviaQuestions.count)
-        var questionDictionary = [String:String]()
+    func startTimer(){
+        //Starts the timer sets the decrement count to 1 and sets the text of the timer label
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer.text = String ("Timer: \(timerCounter)")
+    }
+    
+    func updateTimer(){
+        //Decrements timer by count by 1
+        timerCounter = timerCounter - 1
+        timer.text = String("Timer: \(timerCounter)")
         
-        totalNumOfQuestionsUsed += 1
-        print(usedQuestions)
-        print("QUESTION SELECTED: \(indexOfSelectedQuestion)")
-        
-        while usedQuestions.contains(indexOfSelectedQuestion){
-            print("THIS QUESTION WAS USED ALREADY GETTING A NEW ONE")
-           indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(questionSet.triviaQuestions.count)
-            print("NEW QUESTION IS NOW \(indexOfSelectedQuestion)")
-            if (totalNumOfQuestionsUsed == 1 || totalNumOfQuestionsUsed == questionSet.triviaQuestions.count)
-            {
-                print("THIS IS THE FIRST QUESTION")
-                questionDictionary = questionSet.triviaQuestions[indexOfSelectedQuestion]
-                break
-            }
+        //If timer runs out stop the timer and display the right answer
+        if timerCounter == 0{
+            stopTimer()
+            setGameScreen(sound: audioPlayer2, hideCorrectAnswerField: false, setCorrectAnswerText: "Sorry! You ran out of time the correct answer was ", setQuetionFieldText: " ", setDelayTime: 3)
+            settings.questionsAsked += 1
         }
+    }
+    
+    func stopTimer(){
+        //Stops and resets timer ot 15 seconds
+        gameTimer.invalidate()
+        timerCounter = 15
+        timer.text = String("Timer: \(timerCounter)")
+    }
+    
+    func generateRandomNumber(upperBound upperBound:Int)->Int{
+        //Creates and returns a random number
+        let randomNumber = GKRandomSource.sharedRandom().nextIntWithUpperBound(upperBound)
+        return randomNumber
+    }
+    
+    func displayQuestion() {
+        //Generates a random number to select a question that has not been used yet. If a question that has been
+        //used is selected it will generate a new one until that is not the case
+        indexOfSelectedQuestion = generateRandomNumber(upperBound: questionSet.triviaQuestions.count)
+        while usedQuestions.contains(indexOfSelectedQuestion){
+           indexOfSelectedQuestion = generateRandomNumber(upperBound: questionSet.triviaQuestions.count)
+        }
+        
         usedQuestions.append(indexOfSelectedQuestion)
-        questionDictionary = questionSet.triviaQuestions[indexOfSelectedQuestion]
+        let questionDictionary = questionSet.triviaQuestions[indexOfSelectedQuestion]
         questionField.text = questionDictionary["Question"]
         playAgainButton.hidden = true
     }
-
-
     
+    func displayAnswerOptions(){
+        let selectedQuestionDict = questionSet.triviaQuestions[indexOfSelectedQuestion]
+        var options: [String] = ["Answer","Option2","Option3","Option4"]
+        var randomNumber = generateRandomNumber(upperBound: options.count)
+        
+        usedOptions.append(randomNumber)
+        
+        //Cycles through the number of choices in the choices array and randomly orders the choice options
+        //on the choice buttons so that they are dynamically placed and not in the same spot every time
+        for i in 0..<choices.count{
+            choices[i].setTitle(selectedQuestionDict[options[randomNumber]], forState: UIControlState.Normal)
+            randomNumber = generateRandomNumber(upperBound: options.count)
+            
+            //If an randomly selected option is already displayed on a choice button select a different one until
+            //that is not the case or until all the choices have been used
+            while usedOptions.contains(randomNumber)&&usedOptions.count != choices.count{
+                randomNumber = generateRandomNumber(upperBound: options.count)
+            }
+            //Add used choices to the array
+            usedOptions.append(randomNumber)
+        }
+        //Clear the used choices array for the next question
+        usedOptions = []
+    }
+
     func displayScore() {
         // Hide the answer buttons
         for choice in choices{
@@ -86,36 +132,55 @@ class ViewController: UIViewController {
         
         // Display play again button
         playAgainButton.hidden = false
-        
         questionField.text = "Way to go!\nYou got \(settings.correctQuestions) out of \(settings.questionsPerRound) correct!"
-        
     }
     
     @IBAction func checkAnswer(sender: UIButton) {
         // Increment the questions asked counter
         settings.questionsAsked += 1
         
-        let selectedQuestionDict = questionSet.triviaQuestions[indexOfSelectedQuestion]
-        let correctAnswer = selectedQuestionDict["Answer"]
-        
-        if (sender === choice1 &&  correctAnswer == "True") || (sender === choice2 && correctAnswer == "False") {
+        //If selected choice button text matches the correct answer text then display correct game screen if not display
+        //the incorrect selection game screen
+        if (sender.titleLabel!.text == getCorrectAnswer()) {
+            stopTimer()
+            setGameScreen(sound: audioPlayer, hideCorrectAnswerField: true, setCorrectAnswerText: " ", setQuetionFieldText: "Correct!", setDelayTime: 2)
             settings.correctQuestions += 1
-            questionField.text = "Correct!"
         } else {
-            questionField.text = "Sorry, wrong answer!"
+            stopTimer()
+            setGameScreen(sound: audioPlayer2, hideCorrectAnswerField: false, setCorrectAnswerText: "Sorry! The correct answer was actually: ", setQuetionFieldText: " ", setDelayTime: 3)
         }
-        
-        loadNextRoundWithDelay(seconds: 2)
     }
     
     func nextRound() {
         if settings.questionsAsked == settings.questionsPerRound {
             // Game is over
             displayScore()
+            displayCorrectAnswerField.hidden = true
+            timer.hidden = false
+            timer.text = " "
         } else {
             // Continue game
+            startTimer()
+            displayCorrectAnswerField.hidden = true
             displayQuestion()
+            displayAnswerOptions()
         }
+    }
+    
+    func setGameScreen(sound sound:AVAudioPlayer, hideCorrectAnswerField: Bool, setCorrectAnswerText: String, setQuetionFieldText:String, setDelayTime:Int){
+        //Sets the game sound and text labels based on whether or not the correct or wrong answer was selected
+        sound.play()
+        displayCorrectAnswerField.hidden = hideCorrectAnswerField
+        displayCorrectAnswerField.text = "\(setCorrectAnswerText) \(getCorrectAnswer())"
+        questionField.text = setQuetionFieldText
+        loadNextRoundWithDelay(seconds: setDelayTime)
+    }
+    
+    func getCorrectAnswer()->String{
+        //Gets the correct answer for selected question and returns it
+        let selectedQuestionDict = questionSet.triviaQuestions[indexOfSelectedQuestion]
+        let correctAnswer = selectedQuestionDict["Answer"]
+        return correctAnswer!
     }
     
     @IBAction func playAgain() {
@@ -123,7 +188,7 @@ class ViewController: UIViewController {
         for choice in choices{
           choice.hidden = false
         }
-        
+        //Resets these fields
         settings.questionsAsked = 0
         settings.correctQuestions = 0
         nextRound()
@@ -149,7 +214,17 @@ class ViewController: UIViewController {
         let pathToSoundFile = NSBundle.mainBundle().pathForResource("GameSound", ofType: "wav")
         let soundURL = NSURL(fileURLWithPath: pathToSoundFile!)
         AudioServicesCreateSystemSoundID(soundURL, &settings.gameSound)
+        
+        do{
+        try audioPlayer = AVAudioPlayer(contentsOfURL: correctSoundURL, fileTypeHint: nil)
+        try audioPlayer2 = AVAudioPlayer(contentsOfURL: wrongSoundURL, fileTypeHint: nil)
+        }catch{
+            print("SOUND ERROR")
+        }
+        audioPlayer.prepareToPlay()
+        audioPlayer2.prepareToPlay()
     }
+    
     
     func playGameStartSound() {
         AudioServicesPlaySystemSound(settings.gameSound)
